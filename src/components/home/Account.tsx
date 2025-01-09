fcb0a96f6495b98cc811be853790b55c96055b51
jiwwonnn <grayslacks@boomco.org>
2025-01-09 17:45:11 +0900

    3

--- src/components/home/Account.tsx
+++ src/components/home/Account.tsx
@@ -1,6 +1,51 @@
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import Button from '@shared/Button'
+import Spacing from '@shared/Spacing'
+import Image from "next/Image";
+
 const Account = () => {
+
+  const hasAccount = false
+
+  // 계좌를 보유중
+  if (hasAccount) {
+    return (
+      <div style={{ padding: 24}}>
+        <Flex justify={'space-between'} align={'center'}>
+          <Flex direction={'column'}>
+            <Text typography={'t6'} color={'gray600'}>text 님의 자산</Text>
+            <Spacing size={2} />
+            <Text typography={'t3'} bold={true}>7,000원</Text>
+          </Flex>
+          <Button>분석</Button>
+        </Flex>
+      </div>
+    )
+  }
+
+  // 계좌를 보유하고 있지 않다 > 계조를 개설중일수도있음 , 하지만 개설이 완료되지는 않았다.
+
+  // READY || DONNE
+  const 계좌개설상태 = 'READY'
+  const title = 계좌개설상태 === 'READY' ? "만들고 있으신\n계좌가 있으시군요" : "계좌 개설이\n 더 쉽고 빨라졌어요"
+  const buttonLabel = 계좌개설상태 === 'READY' ? '이어만들기' : '3분만에 개설하기'
   return (
-    <div></div>
+    <div style={{ padding: 24}}>
+      <Flex justify={'space-between'}>
+        <Flex direction={'column'}>
+          <Text bold={true} style={{ whiteSpace : 'pre-wrap'}}>{title}</Text>
+          <Spacing size={8} />
+          <Button>{buttonLabel}</Button>
+        </Flex>
+        <Image
+          src="https://cdn4.iconfinder.com/data/icons/business-and-finance-colorful-free-hand-drawn-set/100/money_dollars-512.png"
+          alt=""
+          width={80}
+          height={80}
+        />
+      </Flex>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,28 @@ import dynamic from 'next/dynamic'
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
 import Skeleton from "@shared/Skeleton";
+import Account from '@components/home/Account'
+import {BannerSkeleton} from "@components/home/EventBanners";
+import Spacing from "@shared/Spacing";
 
 const EventBanners = dynamic(() => import("@components/home/EventBanners"),{
   ssr: false,
-  loading : () => <Skeleton width={'100%'} height={100} style={{borderRadius: 8}}/>
+  loading : () => <BannerSkeleton />
+})
+
+const CreditScore = dynamic(() => import("@components/home/CreditScore"),{
+  ssr: false,
+  loading : () => null
 })
 
 export default function Home() {
   return (
     <>
-      <Container>
-        <EventBanners />
-        <div css={bold}>hello</div>
-      </Container>
-
+      <EventBanners />
+      <Account />
+      <Spacing size={8} backgroundColor={'gray100'} />
+      <CreditScore />
     </>
   );
 }
 
-const Container = styled.div`
-  background-color: pink;
-`
-
-const bold = css`
-  font-weight: bold
-`
